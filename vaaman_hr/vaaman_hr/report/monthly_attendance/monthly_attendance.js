82a521631e47aed26e4c40e929e2cc8ed9748d46
Pratul <ptpratul2@gmail.com>
2024-07-03 10:35:12 +0530

    Monthly Attendance Report Fix

--- vaaman_hr/vaaman_hr/report/monthly_attendance/monthly_attendance.js
+++ vaaman_hr/vaaman_hr/report/monthly_attendance/monthly_attendance.js
@@ -1,57 +1,109 @@
+// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// License: GNU General Public License v3. See license.txt
+
 frappe.query_reports["Monthly Attendance"] = {
-    "filters": [
+	filters: [
+		{
+			fieldname: "month",
+			label: __("Month"),
+			fieldtype: "Select",
+			reqd: 1,
+			options: [
+				{ value: 1, label: __("Jan") },
+				{ value: 2, label: __("Feb") },
+				{ value: 3, label: __("Mar") },
+				{ value: 4, label: __("Apr") },
+				{ value: 5, label: __("May") },
+				{ value: 6, label: __("June") },
+				{ value: 7, label: __("July") },
+				{ value: 8, label: __("Aug") },
+				{ value: 9, label: __("Sep") },
+				{ value: 10, label: __("Oct") },
+				{ value: 11, label: __("Nov") },
+				{ value: 12, label: __("Dec") },
+			],
+			default: frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth() + 1,
+		},
+		{
+			fieldname: "year",
+			label: __("Year"),
+			fieldtype: "Select",
+			reqd: 1,
+		},
+		{
+			fieldname: "employee",
+			label: __("Employee"),
+			fieldtype: "Link",
+			options: "Employee",
+			get_query: () => {
+				var company = frappe.query_report.get_filter_value("company");
+				return {
+					filters: {
+						company: company,
+					},
+				};
+			},
+		},
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1,
+		},
         {
-            "fieldname": "employee",
-            "label": __("Employee"),
+            "fieldname": "branch",
+            "label": __("Branch"),
             "fieldtype": "Link",
-            "options": "Employee",
-            "default": frappe.defaults.get_user_default("Employee")
-        },
-        {
-            "fieldname": "month",
-            "label": __("Month"),
-            "fieldtype": "Select",
-            "options": [
-                { "label": __("January"), "value": "1" },
-                { "label": __("February"), "value": "2" },
-                { "label": __("March"), "value": "3" },
-                { "label": __("April"), "value": "4" },
-                { "label": __("May"), "value": "5" },
-                { "label": __("June"), "value": "6" },
-                { "label": __("July"), "value": "7" },
-                { "label": __("August"), "value": "8" },
-                { "label": __("September"), "value": "9" },
-                { "label": __("October"), "value": "10" },
-                { "label": __("November"), "value": "11" },
-                { "label": __("December"), "value": "12" }
-            ],
-            "default": (new Date()).getMonth() + 1
-        },
-        {
-            "fieldname": "year",
-            "label": __("Year"),
-            "fieldtype": "Int",
-            "default": (new Date()).getFullYear()
+            "options": "Branch"
         },
 		{
-            "fieldname": "employee_group",
-            "label": __("Employee Group"),
-            "fieldtype": "Link",
-            "options": "Employee Group"
-        },
-        {
-            "fieldname": "department",
-            "label": __("Department"),
-            "fieldtype": "Link",
-            "options": "Department"
-        },
-        {
-            "fieldname": "company",
-            "label": __("Company"),
-            "fieldtype": "Link",
-            "options": "Company",
-            "default": frappe.defaults.get_user_default("Company")
-        },
-    ]
-}
+			fieldname: "group_by",
+			label: __("Group By"),
+			fieldtype: "Select",
+			options: ["", "Branch", "Grade", "Department", "Designation"],
+		},
+		{
+			fieldname: "summarized_view",
+			label: __("Summarized View"),
+			fieldtype: "Check",
+			Default: 0,
+		},
+	],
+	onload: function () {
+		return frappe.call({
+			method: "hrms.hr.report.monthly_attendance_sheet.monthly_attendance_sheet.get_attendance_years",
+			callback: function (r) {
+				var year_filter = frappe.query_report.get_filter("year");
+				year_filter.df.options = r.message;
+				year_filter.df.default = r.message.split("\n")[0];
+				year_filter.refresh();
+				year_filter.set_input(year_filter.df.default);
+			},
+		});
+	},
+	formatter: function (value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		const summarized_view = frappe.query_report.get_filter_value("summarized_view");
+		const group_by = frappe.query_report.get_filter_value("group_by");
+
+		if (group_by && column.colIndex === 1) {
+			value = "<strong>" + value + "</strong>";
+		}
+
+		if (!summarized_view) {
+			if ((group_by && column.colIndex > 3) || (!group_by && column.colIndex > 2)) {
+				if (value == "P" || value == "WFH")
+					value = "<span style='color:green'>" + value + "</span>";
+				else if (value == "A") value = "<span style='color:red'>" + value + "</span>";
+				else if (value == "HD") value = "<span style='color:orange'>" + value + "</span>";
+				else if (value == "L") value = "<span style='color:#318AD8'>" + value + "</span>";
+			}
+		}
+
+		return value;
+	},
+};
+
 
