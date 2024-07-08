d02562a2239d47c246fd31d3b92cf44618a30c63
Pratul <ptpratul2@gmail.com>
2024-07-08 14:32:38 +0530

    shift attendance report updated

--- vaaman_hr/vaaman_hr/report/shift_attendance_for_employee/shift_attendance_for_employee.py
+++ vaaman_hr/vaaman_hr/report/shift_attendance_for_employee/shift_attendance_for_employee.py
@@ -23,13 +23,13 @@ def get_columns():
 			"fieldname": "employee",
 			"fieldtype": "Link",
 			"options": "Employee",
-			"width": 220,
+			"width": 100,
 		},
 		{
 			"fieldname": "employee_name",
 			"fieldtype": "Data",
 			"label": _("Employee Name"),
-			"width": 0,
+			"width": 100,
 			"hidden": 1,
 		},
 		{
@@ -37,13 +37,13 @@ def get_columns():
 			"fieldname": "shift",
 			"fieldtype": "Link",
 			"options": "Shift Type",
-			"width": 120,
+			"width": 50,
 		},
 		{
 			"label": _("Attendance Date"),
 			"fieldname": "attendance_date",
 			"fieldtype": "Date",
-			"width": 130,
+			"width": 100,
 		},
 		{
 			"label": _("Status"),
@@ -53,14 +53,14 @@ def get_columns():
 		},
 		{
 			"label": _("Shift Start Time"),
-			"fieldname": "shift_start",
-			"fieldtype": "Data",
+			"fieldname": "start_time",
+			"fieldtype": "Time",
 			"width": 125,
 		},
 		{
 			"label": _("Shift End Time"),
-			"fieldname": "shift_end",
-			"fieldtype": "Data",
+			"fieldname": "end_time",
+			"fieldtype": "Time",
 			"width": 125,
 		},
 		{
@@ -79,25 +79,20 @@ def get_columns():
 			"label": _("Total Working Hours"),
 			"fieldname": "working_hours",
 			"fieldtype": "Data",
-			"width": 100,
+			"width": 50,
 		},
 		{
-			"label": _("Late Entry By"),
-			"fieldname": "late_entry_hrs",
+			"label": _("Over Time"),
+			"fieldname": "custom_over_time",
 			"fieldtype": "Data",
-			"width": 120,
+			"width": 50,
 		},
+	
 		{
-			"label": _("Early Exit By"),
-			"fieldname": "early_exit_hrs",
-			"fieldtype": "Data",
-			"width": 120,
-		},
-		{
-			"label": _("Department"),
-			"fieldname": "department",
+			"label": _("Branch"),
+			"fieldname": "branch",
 			"fieldtype": "Link",
-			"options": "Department",
+			"options": "Branch",
 			"width": 150,
 		},
 		{
@@ -107,18 +102,7 @@ def get_columns():
 			"options": "Company",
 			"width": 150,
 		},
-		{
-			"label": _("Shift Actual Start Time"),
-			"fieldname": "shift_actual_start",
-			"fieldtype": "Data",
-			"width": 165,
-		},
-		{
-			"label": _("Shift Actual End Time"),
-			"fieldname": "shift_actual_end",
-			"fieldtype": "Data",
-			"width": 165,
-		},
+
 		{
 			"label": _("Attendance ID"),
 			"fieldname": "name",
@@ -149,11 +133,7 @@ def get_report_summary(data):
 			half_day_records += 1
 		else:
 			absent_records += 1
-
-		if entry.late_entry:
-			late_entries += 1
-		if entry.early_exit:
-			early_exits += 1
+		
 
 	return [
 		{
@@ -174,18 +154,7 @@ def get_report_summary(data):
 			"label": _("Absent Records"),
 			"datatype": "Int",
 		},
-		{
-			"value": late_entries,
-			"indicator": "Red",
-			"label": _("Late Entries"),
-			"datatype": "Int",
-		},
-		{
-			"value": early_exits,
-			"indicator": "Red",
-			"label": _("Early Exits"),
-			"datatype": "Int",
-		},
+	
 	]
 
 
@@ -210,11 +179,13 @@ def get_chart_data(data):
 
 
 def get_query(filters):
-	attendance = frappe.qb.DocType("Attendance")
+	attendance = frappe.qb.DocType("Attendance")	
 	shift_type = frappe.qb.DocType("Shift Type")
 
 	query = (
 		frappe.qb.from_(attendance)
+		# .inner_join(checkin)
+		# .on(checkin.attendance == attendance.name)
 		.inner_join(shift_type)
 		.on(attendance.shift == shift_type.name)
 		.select(
@@ -224,19 +195,15 @@ def get_query(filters):
 			attendance.shift,
 			attendance.attendance_date,
 			attendance.status,
+			shift_type.start_time,
+    		shift_type.end_time,
 			attendance.in_time,
 			attendance.out_time,
 			attendance.working_hours,
-			attendance.late_entry,
-			attendance.early_exit,
-			attendance.department,
+			attendance.custom_over_time,			
+			attendance.custom_branch,			
 			attendance.company,
-			shift_type.start_time,
-			shift_type.end_time,			
-			shift_type.enable_late_entry_marking,
-			shift_type.late_entry_grace_period,
-			shift_type.enable_early_exit_marking,
-			shift_type.early_exit_grace_period,
+						
 		)
 		.where(attendance.docstatus == 1)
 		.groupby(attendance.name)
@@ -248,11 +215,7 @@ def get_query(filters):
 		elif filter == "to_date":
 			query = query.where(attendance.attendance_date <= filters.to_date)
 		elif filter == "consider_grace_period":
-			continue
-		elif filter == "late_entry" and not filters.consider_grace_period:
-			query = query.where(attendance.in_time > shift_type.shift_start)
-		elif filter == "early_exit" and not filters.consider_grace_period:
-			query = query.where(attendance.out_time < shift_type.shift_end)
+			continue	
 		else:
 			query = query.where(attendance[filter] == filters[filter])
 
@@ -261,16 +224,11 @@ def get_query(filters):
 
 def update_data(data, filters):
 	for d in data:
-		update_late_entry(d, filters.consider_grace_period)
-		update_early_exit(d, filters.consider_grace_period)
-
 		d.working_hours = format_float_precision(d.working_hours)
+		d.custom_over_time = format_float_precision(d.custom_over_time)
 		d.in_time, d.out_time = format_in_out_time(d.in_time, d.out_time, d.attendance_date)
-		d.shift_start, d.shift_end = convert_datetime_to_time_for_same_date(d.shift_start, d.shift_end)
-		d.shift_actual_start, d.shift_actual_end = convert_datetime_to_time_for_same_date(
-			d.shift_actual_start, d.shift_actual_end
-		)
 	return data
+
 
 
 def format_float_precision(value):
@@ -296,29 +254,3 @@ def convert_datetime_to_time_for_same_date(start, end):
 		start = format_datetime(start)
 		end = format_datetime(end)
 	return start, end
-
-
-def update_late_entry(entry, consider_grace_period):
-	if consider_grace_period:
-		if entry.late_entry:
-			entry_grace_period = entry.late_entry_grace_period if entry.enable_late_entry_marking else 0
-			start_time = entry.shift_start + timedelta(minutes=entry_grace_period)
-			entry.late_entry_hrs = entry.in_time - start_time
-	elif entry.in_time and entry.in_time > entry.shift_start:
-		entry.late_entry = 1
-		entry.late_entry_hrs = entry.in_time - entry.shift_start
-	if entry.late_entry_hrs:
-		entry.late_entry_hrs = format_duration(entry.late_entry_hrs.total_seconds())
-
-
-def update_early_exit(entry, consider_grace_period):
-	if consider_grace_period:
-		if entry.early_exit:
-			exit_grace_period = entry.early_exit_grace_period if entry.enable_early_exit_marking else 0
-			end_time = entry.shift_end - timedelta(minutes=exit_grace_period)
-			entry.early_exit_hrs = end_time - entry.out_time
-	elif entry.out_time and entry.out_time < entry.shift_end:
-		entry.early_exit = 1
-		entry.early_exit_hrs = entry.shift_end - entry.out_time
-	if entry.early_exit_hrs:
-		entry.early_exit_hrs = format_duration(entry.early_exit_hrs.total_seconds())
